eb71ec45897f30d57495c40f0548264ac0829b9d
Yolisa Qadi <167926925+Yolisaq@users.noreply.github.com>
2025-09-19 03:34:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 20px;
+    background-color: #f9f9f9;
+    color: #333;
+    transition: background-color 0.3s, color 0.3s;
+}
+
+.dark-mode {
+    background-color: #333;
+    color: #f9f9f9;
+}
+
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 40px;
+}
+
+button {
+    padding: 10px 20px;
+    cursor: pointer;
+}
+
+.faq-answer {
+    display: none;
+    margin-left: 20px;
+}
+
+.error {
+    color: red;
+    font-size: 0.9em;
+}
+
+.success {
+    color: green;
+    font-size: 1em;
+    margin-top: 10px;
+}
